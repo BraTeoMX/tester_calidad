5020c96e5ca9532448b9360a0911a9b37e51dc1b
Brayam Teofilo Jimenez <brayamteo@gmail.com>
2024-06-01 21:41:35 -0600

    adiciones al proyecto

---
+++ storage/framework/views/20318bd7650e7ba90bac892079aec2e2d38bbfec.php
@@ -0,0 +1,8 @@
+<?php if(auth()->guard()->check()): ?>
+    <?php echo $__env->make('layouts.navbars.navs.auth', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php endif; ?>
+
+<?php if(auth()->guard()->guest()): ?>
+    <?php echo $__env->make('layouts.navbars.navs.guest', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php endif; ?>
+<?php /**PATH /Users/mac/proyectos-laravel/calidad_testeoxD/tester_calidad/resources/views/layouts/navbars/navbar.blade.php ENDPATH**/ ?>
