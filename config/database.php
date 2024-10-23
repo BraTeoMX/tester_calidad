5f6f08ff174e8f6cd93e64a93a709bf5219f2e83
Adolfo <adolfodjhz@gmail.com>
2024-10-23 13:14:54 -0600

    update

--- config/database.php
+++ config/database.php
@@ -63,17 +63,6 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_SQLSRV_HOST', 'localhost'),
-            'port' => env('DB_SQLSRV_PORT', '1433'),
-            'database' => env('DB_SQLSRV_DATABASE', 'forge'),
-            'username' => env('DB_SQLSRV_USERNAME', 'forge'),
-            'password' => env('DB_SQLSRV_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
             'sqlsrv' => [
                 'driver' => 'sqlsrv',
                 'host' => env('DB_SQLSRV_HOST', 'localhost'),
