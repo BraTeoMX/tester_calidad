c5cfd797d230665376e1110fce3b190034129ff5
BraTeoMX <brateomx@gmail.com>
2026-01-19 18:01:31 -0600

    ajuste para validaciones correctas de los horarios

--- resources/views/turnos/index.blade.php
+++ resources/views/turnos/index.blade.php
@@ -88,7 +88,7 @@
                                     <input type="time" name="horarios[{{ $num }}][inicio]" id="inicio_{{ $num }}" class="form-control" style="color: black;">
                                 </td>
                                 <td>
-                                    <input type="time" name="horarios[{{ $num }}][fin]" id="fin_{{ $num }}" class="form-control" style="color: black;">
+                                    <input type="time" name="horarios[{{ $num }}][fin]" id="fin_{{ $num }}" class="form-control" style="color: black;" disabled>
                                 </td>
                             </tr>
                             @endforeach
@@ -168,13 +168,83 @@
     .custom-modal-content input.form-control:focus {
         border-color: #e14eca;
     }
+
+    /* Asegurar que SweetAlert aparezca por encima del modal */
+    .swal2-container {
+        z-index: 10000 !important;
+    }
 </style>
 
 <script>
+    // Event Listeners para Validación de Horarios
+    $(document).ready(function() {
+        // Al cambiar cualquier input de inicio
+        $('input[name*="[inicio]"]').on('change', function() {
+            let inicioInput = $(this);
+            let dia = inicioInput.attr('id').split('_')[1]; // obtener numero de dia
+            let finInput = $('#fin_' + dia);
+            let horaInicio = inicioInput.val();
+
+            if (horaInicio) {
+                // Habilitar fin
+                finInput.prop('disabled', false);
+                // Establecer mínimo para fin (aunque input type time no soporta min dinámico en todos los browsers igual que date, ayuda)
+                // Para validación real usamos JS en el change del fin.
+                finInput.attr('min', horaInicio);
+
+                // Si ya había un valor en fin y es menor o igual, limpiarlo o validar
+                if (finInput.val() && finInput.val() <= horaInicio) {
+                    // Opción: Limpiar para obligar a re-seleccionar
+                    finInput.val('');
+                    Swal.fire({
+                        toast: true,
+                        position: 'top-end',
+                        icon: 'warning',
+                        title: 'La hora fin se ha reiniciado porque debe ser mayor a la hora de inicio.',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                }
+            } else {
+                // Si se borra el inicio, limpiar y deshabilitar fin
+                finInput.val('');
+                finInput.prop('disabled', true);
+            }
+        });
+
+        // Al cambiar cualquier input de fin
+        $('input[name*="[fin]"]').on('change', function() {
+            let finInput = $(this);
+            let dia = finInput.attr('id').split('_')[1];
+            let inicioInput = $('#inicio_' + dia);
+
+            let horaInicio = inicioInput.val();
+            let horaFin = finInput.val();
+
+            if (horaFin && horaInicio) {
+                if (horaFin <= horaInicio) {
+                    // Hora inválida
+                    finInput.val(''); // Limpiar valor incorrecto
+                    Swal.fire({
+                        toast: true,
+                        position: 'top-end',
+                        icon: 'error',
+                        title: 'La hora de fin debe ser posterior a la hora de inicio.',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                }
+            }
+        });
+    });
+
     function openCreateModal() {
         $('#turnoForm')[0].reset();
         $('#turno_id').val('');
         $('#turnoModalLabel').text('Crear Nuevo Turno');
+
+        // Resetear todos los campos 'fin' a disabled
+        $('input[name*="[fin]"]').prop('disabled', true);
 
         // Custom show logic
         $('#customModal').fadeIn(200);
@@ -194,11 +264,23 @@
             $('#nombre').val(data.nombre);
             $('#turnoModalLabel').text('Editar Turno: ' + data.nombre);
 
+            // Resetear primero todos a disabled
+            $('input[name*="[fin]"]').prop('disabled', true);
+
             if (data.horario_semanal) {
                 // Iterar 1 al 7
                 for (let i = 1; i <= 7; i++) {
                     if (data.horario_semanal[i]) {
+                        // Set Inicio
                         $('#inicio_' + i).val(data.horario_semanal[i].inicio);
+
+                        // Si hay inicio, habilitar fin
+                        if (data.horario_semanal[i].inicio) {
+                            $('#fin_' + i).prop('disabled', false);
+                            $('#fin_' + i).attr('min', data.horario_semanal[i].inicio);
+                        }
+
+                        // Set Fin
                         $('#fin_' + i).val(data.horario_semanal[i].fin);
                     }
                 }
@@ -214,6 +296,29 @@
 
     function submitTurno(e) {
         e.preventDefault();
+
+        // VALIDACIÓN CLIENT-SIDE EXTRA
+        // Verificar que si hay inicio, haya fin seleccionada
+        let isValid = true;
+        $('input[name*="[inicio]"]').each(function() {
+            let startVal = $(this).val();
+            if (startVal) {
+                let dia = $(this).attr('id').split('_')[1];
+                let endVal = $('#fin_' + dia).val();
+
+                if (!endVal) {
+                    isValid = false;
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Falta hora de fin',
+                        text: 'Has seleccionado una hora de inicio para el ' + getNombreDia(dia) + ' pero falta la hora de fin.',
+                    });
+                    return false; // Break loop
+                }
+            }
+        });
+
+        if (!isValid) return;
 
         let id = $('#turno_id').val();
         let url = id ? '/turnos/' + id : '/turnos';
@@ -248,6 +353,19 @@
                 Swal.fire('Error', errorMessage, 'error');
             }
         });
+    }
+
+    function getNombreDia(num) {
+        const dias = {
+            1: 'Lunes',
+            2: 'Martes',
+            3: 'Miércoles',
+            4: 'Jueves',
+            5: 'Viernes',
+            6: 'Sábado',
+            7: 'Domingo'
+        };
+        return dias[num] || 'Día ' + num;
     }
 
     // Cerrar modal al hacer clic fuera del contenido
